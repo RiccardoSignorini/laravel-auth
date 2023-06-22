05f2c475612acfc884f6914a9f4123a241415d5f
RiccardoSignorini <riccardo.signorini1@gmail.com>
2023-06-22 17:24:09 +0200

    cambio indirizzo + modifiche

--- resources/views/pages/works/show.blade.php
+++ resources/views/pages/works/show.blade.php
@@ -1,7 +1,7 @@
-@extends('welcome')
+@extends('layouts.app')
 
 @section('title')
-    PIZZA | Show
+    WORKS | Show
 @endsection
 
 @section('content')
@@ -12,13 +12,15 @@
 
         <div class="d-flex flex-wrap container text-center justify-content-around p-5">
             {{-- SINGOLA CARD --}}
-            <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">{{$single_pizza['nome_pizza']}}</h5>
-                    <p class="card-text">{{$single_pizza['ingredienti']}}</p>
+            <div class="card col-lg-2 text-center mx-4 my-4">
+                <img src="{{ $single_work->image }}" alt="">
+                <div class="icon my-3">
+                    <h5>{{ $single_work->title }}</h5>
+                    <p>{{ $single_work->lenguages }}</p>
+                    <p>{{ $single_work->data_create }}</p>
                     {{-- BUTTON EDIT --}}
-                    <a class="btn btn-primary" href="{{route('pizzas.edit', $single_pizza)}}">Change</a>
-                    <form action="{{route('pizzas.destroy', $single_pizza)}}" method="POST" class="d-inline">
+                    <a class="btn btn-primary" href="{{route('works.edit', $single_work)}}">Change</a>
+                    <form action="{{route('works.destroy', $single_work)}}" method="POST" class="d-inline">
                         @csrf
                         @method('DELETE')
                         {{-- BUTTON DELETE --}}
@@ -33,8 +35,8 @@
     <script>
 
         function confirmDelete(){
-            return confirm('Are you sure you want delete?')
-        }
+            return confirm('Are you sure that you want delete?')
+        } 
 
     </script>
 
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WorkController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.works.index');
 });
 
 Route::middleware('auth')->group(function () {
